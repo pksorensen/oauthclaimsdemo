7f0c894b73fafc328aa9932112b7839a9482557f
Poul Sørensen <poulmobil@gmail.com>
2013-04-26 19:47:42 +0200

    Last Commit

--- OauthClaimsDemo/Controllers/HomeController.cs
+++ OauthClaimsDemo/Controllers/HomeController.cs
@@ -9,28 +9,7 @@ namespace OauthClaimsDemo.Controllers
 {
     public class HomeController : Controller
     {
-        static HomeController()
-        {
-            RegisterOAuth2Clients();
-        }
-
-        static void RegisterOAuth2Clients()
-        {
-            OAuth2Client.Instance.RegisterProvider(
-                ProviderType.Google,
-                "539276601595.apps.googleusercontent.com",
-                "RbE5_zZ40f0yMYSJaxNkUUPp");
-
-            OAuth2Client.Instance.RegisterProvider(
-                ProviderType.Facebook,
-                "195156077252380",
-                "39b565fd85265c56010555f670573e28");
-
-            OAuth2Client.Instance.RegisterProvider(
-                ProviderType.Live,
-                "00000000400DF045",
-                "4L08bE3WM8Ra4rRNMv3N--un5YOBr4gx");
-        }
+      
 
         public ActionResult Index()
         {
